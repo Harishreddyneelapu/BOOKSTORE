e79105533fcae0814c40b552dd6f4c3e7a81fcf7
Harish Reddy Neelapu <neelapuharishreddy1@gmail.com>
2024-06-01 13:59:13 +0530

    admin registration Api

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -3,6 +3,7 @@ import * as UserService from '../services/user.service';
 
 export const newUserRegister = async (req, res, next) => {
   try {
+    req.body.role = "user";
     const data = await UserService.newUserRegister(req.body);
     const { firstName, lastName, email, role } = data;
         
@@ -25,3 +26,31 @@ export const newUserRegister = async (req, res, next) => {
     });
   }
 };
+
+
+export const newAdminRegister = async (req, res, next) => {
+    try {
+      req.body.role = "admin";
+      const data = await UserService.newUserRegister(req.body);
+      const { firstName, lastName, email, role } = data;
+          
+      res.status(HttpStatus.CREATED).json({
+        code: HttpStatus.CREATED,
+        success: true,
+        message: 'User created successfully',
+        data: {
+          firstName,
+          lastName,
+          email,
+          role
+        },
+      });
+    } catch (error) {
+      res.status(HttpStatus.BAD_REQUEST).json({
+        code: HttpStatus.BAD_REQUEST,
+        success: false,
+        message: `${error}`
+      });
+    }
+  };
+  
--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -6,7 +6,9 @@ import * as validator from '../validators/user.validator';
 const router = express.Router();
 
 
-router.post('', validator.newUserValidatorRegister, userController.newUserRegister);
+router.post('/userRegister', validator.newUserValidatorRegister, userController.newUserRegister);
+
+router.post('/adminRegister', validator.newAdminValidatorRegister, userController.newAdminRegister);
 
 
 export default router;
